b29b421e172ee214548aeea6b9b405528a5cd993
Andrey Allyson <andreyuser47@gmail.com>
2022-12-31 20:13:08 -0300

    [physics] Chunk class has created

--- src/lib/world/physics/index.ts
+++ src/lib/world/physics/index.ts
@@ -2,12 +2,39 @@ import * as thr from 'three';
 import { Cube, CubeTypes, PhysicCube } from './cube';
 
 export class Chunk {
-  
+  static readonly WIDTH = 16;
+  static readonly HEIGHT = 256;
+  static readonly DEPTH = 16;
+
+  private _position: [number, number, number];
+  private _cubes: PhysicCube[];
+
+  constructor(x: number, y: number, z: number) {
+    this._cubes = new Array(Chunk.WIDTH * Chunk.DEPTH * Chunk.HEIGHT);
+
+    this._position = [x, y, z];
+  }
+
+  get position() {
+    return this._position;
+  }
+
+  set(x: number, y: number, z: number, value: PhysicCube) {
+    this._cubes[Chunk.parseIndex(x, y, z)] = value;
+  }
+
+  get(x: number, y: number, z: number): PhysicCube | undefined {
+    return this._cubes[Chunk.parseIndex(x, y, z)];
+  }
+
+  static parseIndex(x: number, y: number, z: number) {
+    return x + y * this.WIDTH + z * this.WIDTH * this.HEIGHT; 
+  }
 }
 
 export class PhysicSpace {
   private _cubes: PhysicCube[];
-  private _chunks: PhysicCube[][];
+  private _chunks: Chunk[];
   private _entities: any[];
   
   constructor() {
@@ -30,7 +57,7 @@ export class PhysicSpace {
     
   }
 
-  createCube(texture: thr.Texture) {
+  createCube(position: thr.Vector3, texture: thr.Texture) {
     const cube = new Cube(texture);
 
     this._cubes.push(cube);
