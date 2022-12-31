e395737bda910b21d9302dbafe06d107001fb7cb
Andrey Allyson <andreyuser47@gmail.com>
2022-12-30 23:11:58 -0300

    [index] modified

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,7 @@
-import * as thr from 'three';
-import { PersonalizedScene } from './sceneCreator';
-import { KeyboardListener, MouseListener } from './userInputs';
+import { Getter } from './lib/inputs/KeyboardListener';
+import { MouseListener } from './lib/inputs/MouseListener';
+import { World } from './lib/world';
 
-const scene = PersonalizedScene.createScene();
-const camera = scene.camera;
+const world = new World();
 
-const mouse = new MouseListener( document.body );
-const keyboard = new KeyboardListener( document.body );
-
-mouse.on('move', (ev, moveX, moveY) => {
-  if (!mouse.clicking) return;
-
-  const worldDirection = camera.getWorldDirection(camera.position.clone());
-
-  camera.rotateOnWorldAxis(new thr.Vector3(0, 1, 0), moveX * -.01);
-  
-  if ((worldDirection.y < .9 || moveY > 0) && (worldDirection.y > -.9 || moveY < 0))
-    camera.rotateX(moveY * -.01);
-});
-
-const cube = scene.createTexturedCube('./Saul.png');
-
-interface IKeyboardHandlers {
-  [k: string]: Function | undefined
-}
-
-const handlers: IKeyboardHandlers = {
-  'p'() {
-    if (scene.paused) {
-      scene.start();
-    } else {
-      scene.pause();
-    }
-  }
-}
-
-keyboard.on('keydown', (ev) => {
-  const handler = handlers[ev.key];
-
-  if (handler) handler();
-});
-
-scene.on('frame', (time) => {
-  const { w, a, s, d } = keyboard.keysNumber;
-
-  cube.rotateX(.01);
-  cube.rotateY(.01);
-
-  const direction = new thr.Vector3(
-    (d - a) * .1,
-    0,
-    (s - w) * .1
-  );
-  
-  direction.applyEuler(camera.rotation);
-
-  camera.position.add(direction);
-});
-
-scene.start();
+world.scene.start();
