67bb0a14aeafc3df6d017fdd9b35df6ae4da24f7
Andrey Allyson <andreyuser47@gmail.com>
2022-12-31 18:45:17 -0300

    [src] index refactored

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,53 @@
-import { Getter } from './lib/inputs/KeyboardListener';
-import { MouseListener } from './lib/inputs/MouseListener';
 import { World } from './lib/world';
+import { MouseListener, KeyboardListener } from './lib/inputs';
+import * as thr from 'three';
 
-const world = new World();
+async function main() {
+  const world = new World();
+  const mouse = new MouseListener( document.body );
+  const keyboard = new KeyboardListener( document.body );
 
-world.scene.start();
+  const camera = world.scene.camera;
+  const CAMERA_MOVEMENT_SPEED = 0.05;
+  const CAMERA_ROTATE_SPEED = 0.01;
+
+  const success = await world.assets.load({
+    textures: World.defaultTextures
+  });
+
+  mouse.on('move', (ev, mx, my) => {
+    if (!mouse.clicking) return;
+
+    camera.rotateX(my * -CAMERA_ROTATE_SPEED);
+    camera.rotateOnWorldAxis(new thr.Vector3(0, 1, 0), mx * -CAMERA_ROTATE_SPEED);
+  });
+
+  keyboard.on('keyup', (ev) => {
+    if (ev.key !== 'p') return;
+    if (world.running) {
+      world.pause();
+    } else {
+      world.start();
+    };
+  });
+
+  world.on('tick', count => {
+    const { w, a, s, d } = keyboard.keysNumber;
+
+    const direction = new thr.Vector3(
+      (d - a) * CAMERA_MOVEMENT_SPEED,
+      0,
+      (s - w) * CAMERA_MOVEMENT_SPEED
+    );
+
+    direction.applyEuler(camera.rotation);
+    
+    camera.position.add(direction);
+  });
+
+  world.generate();
+  
+  world.start();
+}
+
+main();
