b93e4e69e6aa3e18c8bc181acc986fe7870c8314
Andrey Allyson <andreyuser47@gmail.com>
2022-12-31 21:18:37 -0300

    [physics] chunk creation, getter and setter added

--- src/lib/world/physics/index.ts
+++ src/lib/world/physics/index.ts
@@ -1,22 +1,24 @@
 import * as thr from 'three';
 import { Cube, CubeTypes, PhysicCube } from './cube';
 
+export class ChunkLocation extends thr.Vector2 {}
+
 export class Chunk {
   static readonly WIDTH = 16;
   static readonly HEIGHT = 256;
   static readonly DEPTH = 16;
 
-  private _position: [number, number, number];
+  private _location: ChunkLocation;
   private _cubes: PhysicCube[];
 
-  constructor(x: number, y: number, z: number) {
+  constructor(x: number, y: number) {
     this._cubes = new Array(Chunk.WIDTH * Chunk.DEPTH * Chunk.HEIGHT);
 
-    this._position = [x, y, z];
+    this._location = new ChunkLocation(x, y);
   }
 
-  get position() {
-    return this._position;
+  get location() {
+    return this._location;
   }
 
   set(x: number, y: number, z: number, value: PhysicCube) {
@@ -27,19 +29,31 @@ export class Chunk {
     return this._cubes[Chunk.parseIndex(x, y, z)];
   }
 
+  at(index: number) {
+    return this._cubes[index];
+  }
+
   static parseIndex(x: number, y: number, z: number) {
-    return x + y * this.WIDTH + z * this.WIDTH * this.HEIGHT; 
+    return (
+      (x % this.WIDTH ) +
+      (y % this.HEIGHT) * this.WIDTH +
+      (z % this.DEPTH ) * this.WIDTH * this.HEIGHT
+    ); 
+  }
+
+  static Vector2ToChunkLocation(x: number, y: number) {
+    return new ChunkLocation(
+      Math.floor(x / this.WIDTH),
+      Math.floor(y / this.DEPTH)
+    );
   }
 }
 
 export class PhysicSpace {
-  private _cubes: PhysicCube[];
   private _chunks: Chunk[];
   private _entities: any[];
   
   constructor() {
-    this._cubes = [];
-
     this._chunks = [];
 
     this._entities = [];
@@ -49,19 +63,60 @@ export class PhysicSpace {
     return this._chunks;
   }
 
-  get cubes() {
-    return this._cubes;
-  }
-
   update() {
     
   }
 
+  createChunk(x: number, y: number) {
+    const chunk = new Chunk(x, y);
+
+    this._chunks.push(chunk);
+
+    return chunk;
+  }
+
+  getChunkByLocation(searchLocation: ChunkLocation) {
+    for (const chunk of this._chunks) {
+      const equals = chunk.location.x === searchLocation.x && chunk.location.y === searchLocation.y;
+      
+      if (equals)
+        return chunk;
+    }
+  }
+
+  getChunkByVector3(position: thr.Vector3) {
+    const searchLocation = Chunk.Vector2ToChunkLocation(position.x, position.z);
+
+    this.getChunkByLocation(searchLocation);
+  }
+
+  placeInChunk(cube: PhysicCube, force: boolean = false) {
+    const { x, y, z } = cube.position;
+
+    const chunkLocation = Chunk.Vector2ToChunkLocation(x, z);
+
+    let chunk = this.getChunkByLocation(chunkLocation);
+
+    if (!chunk)
+      chunk = this.createChunk(chunkLocation.x, chunkLocation.y)
+
+    if (force || chunk.get(x, y, z))
+      chunk.set(x, y, z, cube);
+  }
+
   createCube(position: thr.Vector3, texture: thr.Texture) {
     const cube = new Cube(texture);
+    
+    const { x, y, z } = position;
+    
+    cube.position.set(x, y, z);
 
-    this._cubes.push(cube);
+    this.placeInChunk(cube);
 
     return cube;
+  }
+
+  removeCube() {
+    
   }
 }
