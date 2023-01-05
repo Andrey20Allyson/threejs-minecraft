152e9f172a80cd24ffb7820de722e4b07a7ae56a
Andrey Allyson <andreyuser47@gmail.com>
2023-01-05 00:37:46 -0300

    update: PhysicSpace now hide colliding cube faces and show when necessary

--- src/lib/world/physics/index.ts
+++ src/lib/world/physics/index.ts
@@ -1,9 +1,20 @@
 import * as thr from 'three';
-import { Cube, CubeTypes, PhysicCube } from './cube';
+import { Cube, CubeFaces as CubeFace, CubeTypes, PhysicCube } from './cube';
 import { Chunk, ChunkLocation } from './chunk';
 import { EventEmitter } from 'events';
 
+export type NeighborInfo = [number, number, number, CubeFace, CubeFace] 
+
 export class PhysicSpace extends EventEmitter {
+  static readonly NEIGHBORS_POSITIONS: NeighborInfo[] = [
+    [ 1, 0, 0, CubeFace.RIGHT, CubeFace.LEFT],
+    [-1, 0, 0, CubeFace.LEFT, CubeFace.RIGHT],
+    [ 0, 1, 0, CubeFace.TOP, CubeFace.BOTTON],
+    [ 0,-1, 0, CubeFace.BOTTON, CubeFace.TOP],
+    [ 0, 0, 1, CubeFace.FRONT, CubeFace.BACK],
+    [ 0, 0,-1, CubeFace.BACK, CubeFace.FRONT]
+  ];
+
   private _chunks: Chunk[];
   private _entities: any[];
   
@@ -62,6 +73,33 @@ export class PhysicSpace extends EventEmitter {
     return this.getChunkByLocation(searchLocation);
   }
 
+  * neighborsOf(position: thr.Vector3): Generator<[PhysicCube, CubeFace, CubeFace]> {
+    const { x, y, z } = position;
+
+    for (const [nx, ny, nz, face, neighborFace] of PhysicSpace.NEIGHBORS_POSITIONS) {
+      const chunk = this.getChunkByLocation(ChunkLocation.fromXY(x + nx, z + nz));
+      if (!chunk) continue;
+
+      const cube = chunk.get(x + nx, y + ny, z + nz);
+      if (!cube) continue;
+
+      yield [cube, face, neighborFace];
+    }
+  }
+
+  hideCollidingFaces(cube: PhysicCube) {
+    for (const [neighborCube, face, neighborFace] of this.neighborsOf(cube.position)) {
+      cube.getFace(face).visible = false;
+
+      neighborCube.getFace(neighborFace).visible = false;
+    }
+  }
+
+  showNeighborFaces(position: thr.Vector3) {
+    for (const [neighborCube, face, neighborFace] of this.neighborsOf(position))
+      neighborCube.getFace(neighborFace).visible = true;
+  }
+
   placeInChunk(cube: PhysicCube, force: boolean = false) {
     const chunkLocation = ChunkLocation.fromVector3(cube.position);
 
@@ -70,23 +108,24 @@ export class PhysicSpace extends EventEmitter {
     if (!chunk)
       chunk = this.createChunkByLocation(chunkLocation);
 
-    const oldCube = chunk.getByVector3(cube.position)
+    const oldCube = chunk.getByVector3(cube.position);
+
+    if (!oldCube) {
+      this.hideCollidingFaces(cube);
+    } else if (force) {
+      this.emit('removed', oldCube);
+    }
 
     if (force || !oldCube) {
       chunk.addCube(cube);
 
       this.emit('added', cube);
     }
-
-    if (force && oldCube)
-      this.emit('removed', oldCube);
   }
 
   addCube(cube: PhysicCube, position?: thr.Vector3) {
-    if (position) {
-      const { x, y, z } = position;
-      cube.position.set(x, y, z);
-    }
+    if (position)
+      cube.position.copy(position);
 
     this.placeInChunk(cube);
 
